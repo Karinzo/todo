96fce1cd5ca71783a1fea28f9622ff7533ae04a7
Karina Kusainova <mrzoo.karina@gmail.com>
2022-05-02 14:57:54 +0400

    extracted function addTask

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ let newTaskInput = document.querySelector("#new-task-input");
 let newTaskAdd = document.querySelector("#new-task-add");
 let tasksList = document.querySelector("#tasks-list");
 
-newTaskAdd.addEventListener('click', function() {    
+function addTask() {
     let listItem = document.createElement('li');
     listItem.textContent = newTaskInput.value;
     tasksList.append(listItem);
     newTaskInput.value = '';
+}
+
+newTaskAdd.addEventListener('click', function() {    
+    addTask();
 });
 
 newTaskInput.addEventListener('keyup', function(e) {
     if (e.key == 'Enter') {
-        let listItem = document.createElement('li');
-        listItem.textContent = newTaskInput.value;
-        tasksList.append(listItem);
-        newTaskInput.value = ''; 
+        addTask();
     }
 });
